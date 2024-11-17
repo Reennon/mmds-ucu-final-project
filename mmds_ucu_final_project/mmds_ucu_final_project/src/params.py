beba72ac9e9f61fff059dfea82587fd49d5d288e
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2024-11-17 15:11:18 +0200

    Feature/spark socket streaming (#2)
    * initial commit on wiki streaming
    
    * initial commit on wiki streaming
    
    * versional
    
    * fixed to not use username for bloomfilter
    
    * added final fields

--- mmds_ucu_final_project/mmds_ucu_final_project/src/params.py
+++ mmds_ucu_final_project/mmds_ucu_final_project/src/params.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 
 
 class Parameters(BaseModel):
-    port: int = 9000
+    port: int = 9001
     bloom_filter_error_rate: float = 0.1
-    min_num_edits: int = 400
+    min_num_edits: int = 40_000
     rdd_sampling: float = 0.2
 
 
